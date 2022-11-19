f119ec638ca2c0856d7b6666b62d08e9670bc232
JLS-DEV-lab <luca@hanjob.de>
2022-11-19 12:38:02 +0100

    comingSoon page

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,39 @@
+import {Helmet} from "react-helmet"
+
+const ComingSoon = () => {
+  return (
+    <div className="App">
+      <Helmet>
+        <title>SOSA - Coming Soon</title>
+        <meta name="description" content="SOSA application" />
+      </Helmet>
+      <div className="container">
+        <div className="logo">SOSA</div>
+        <div className="info">
+          Die Website wird derzeit überarbeitet
+          <h1 className="capitalize">Wir sind demnächst für Sie in <span className="color">Amorbach</span> verfügbar</h1>
+          <h2 className="capitalize margin-top color">shared office solutions amorbach</h2>
+          <div className="more-info margin-top">
+            Weitere Informationen
+            <button type="button">
+              <a href="https://github.com/JLS-DEV-lab/Shelly-based-Heating-control">
+                <div className="link-arrow" />
+              </a>
+            </button>
+          </div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+        
+    </div>
+  );
+}
+
+export default ComingSoon;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ComingSoon from './components/ComingSoon';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ComingSoon />
+  </React.StrictMode>
+);
